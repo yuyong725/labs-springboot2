09c3d68980f7bcba3729b4c3029cf369a2d4fbf7
yuyong <yuyong_material@163.com>
2020-03-11 12:24:52 +0800

    完成prometheus，没什么代码，主要是服务的搭建

---
+++ labs-springboot2-monitor/labs-springboot2-prometheus/labs-springboot2-prometheus-simple/src/main/java/cn/javadog/labs/springboot2/prometheus/simple/PrometheusSimpleApplication.java
@@ -0,0 +1,17 @@
+package cn.javadog.labs.springboot2.prometheus.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 余勇
+ * @date 2020年03月11日 11:41:00
+ */
+@SpringBootApplication
+public class PrometheusSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusSimpleApplication.class, args);
+	}
+
+}
