0258c3a55a75585b82116ed1781ccee23b48a927
yuyong <yuyong_material@163.com>
2020-03-12 18:48:01 +0800

    搞定elk，超爽

---
+++ labs-springboot2-log/labs-springboot2-log-elk/labs-springboot2-log-elk-simple/src/main/java/cn/javadog/labs/springboot2/elk/simple/ElkSimpleApplication.java
@@ -0,0 +1,17 @@
+package cn.javadog.labs.springboot2.elk.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 余勇
+ * @date 2020年03月12日 18:42:00
+ */
+@SpringBootApplication
+public class ElkSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkSimpleApplication.class, args);
+	}
+
+}
